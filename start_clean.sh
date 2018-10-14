65e82a9ad76daecc859b60c104152739565ab55b
root <root@centos-01.stickontheice.com>
2018-10-13 17:33:00 -0700

    added basic .sh

---
+++ start_clean.sh
@@ -0,0 +1,1 @@
+docker run -d --name nagios_clean --hostname nagios4 -p 0.0.0.0:8100:80 -v  jasonrivers/nagios:latest
--- start_nagios_container.sh
+++ start_nagios_container.sh
@@ -1,1 +1,1 @@
-docker run -d --name nagios4 --hostname nagios4 -p 0.0.0.0:8100:80 -v /u01/nagios/etc/:/opt/nagios/etc jasonrivers/nagios:latest
+docker run -d --name nagios4 --hostname nagios4 -p 0.0.0.0:8100:80 -v /u01/docker/nagios/etc/:/opt/nagios/etc jasonrivers/nagios:latest
